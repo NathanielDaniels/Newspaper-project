e7820883a708745bf0f25398baa4693cb590676a
Nathaniel Daniels <26263599+NathanielDaniels@users.noreply.github.com>
2018-12-07 19:15:52 -0600

    lets try this again...

--- server.js
+++ server.js
@@ -14,28 +14,44 @@
 // app.listen(port)
 // // Log to feedback that this is actually running
 // console.log('server started on port ' + port)
+//==============================================================================
+// 'use strict';
 
-'use strict';
+// const express = require('express'),
+//     serveStatic = require('serve-static'),
+//     path = require('path');
 
-const express = require('express'),
-    serveStatic = require('serve-static'),
-    path = require('path');
+// // Instantiate Express App
+// const app = express();
 
-// Instantiate Express App
+// // Middleware
+// app.use('/', serveStatic(path.join(__dirname, '/Newspaper-Project')));
+
+// // Serve Index
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/Newspaper-Project/index.html');
+// });
+
+// // Port
+// const port = process.env.PORT || 5000;
+
+// // Start Server
+// app.listen(port, () => {
+//     console.log(`Server start on port ${port}...`);
+// });
+//================================================
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
 
-// Middleware
-app.use('/', serveStatic(path.join(__dirname, '/Newspaper-Project')));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
 
-// Serve Index
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/Newspaper-Project/index.html');
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-// Port
-const port = process.env.PORT || 5000;
-
-// Start Server
-app.listen(port, () => {
-    console.log(`Server start on port ${port}...`);
-});
+app.listen(port);
